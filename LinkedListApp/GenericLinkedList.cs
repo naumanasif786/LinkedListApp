45643c0be7b412c3ec835b29b837d1ddad701f65
Nauman Asif <mnasif@gmail.com>
2024-09-14 12:36:40 +0100

    Add project files.

---
+++ LinkedListApp/GenericLinkedList.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedListApp
+{
+    public class GenericLinkedList<T>
+    {
+        public Node<T>? _head { get; set; }
+
+        public GenericLinkedList(T data)
+        {
+            Insert(data);
+        }
+
+        public void InsertAt(int position, T data)
+        {
+            if (_head != null)
+            {
+                Node<T> node = _head;
+                for (int i = 0; i < position - 1; i++)
+                {
+                    if (node == null)
+                    {
+                        throw new IndexOutOfRangeException("Position Not Found. Please make sure you insert on the position that exists.");
+                    }
+                    node = node.Next;
+                }
+
+                Console.WriteLine($"Inserting {data} at position { position }.");
+                Node<T> currentNode = node;
+                Node<T> nextNode = node.Next;
+                Node<T> newNode = new Node<T>(data);
+                currentNode.Next = newNode;
+                newNode.Next = nextNode;
+            }
+            
+        }
+
+        public void Insert(T data)
+        {
+            var newNode = new Node<T>(data);
+            if (_head == null)
+                _head = newNode;
+            else
+            {
+                var current = _head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = newNode;
+            }
+        }
+
+        public void RemoveAt(int position)
+        {
+            if (_head == null)
+            {
+                return;
+            }
+
+            Console.WriteLine($"Removing at position {position}");
+
+            Node<T> node = _head;
+
+            if (position == 0)
+            {
+                _head = node.Next;
+                return;
+            }
+            for (int i = 0; node != null && i < position - 1; i++)
+            {
+                node = node.Next;
+            }
+            if (node == null || node.Next == null)
+            {
+                return;
+            }
+
+            // Node node->next is the node to be deleted
+            Node<T> nextNode = node.Next.Next;
+            node.Next = nextNode;
+        }
+
+        public void PrintAll()
+        {
+            Node<T> node = _head;
+            while (node != null) 
+            {
+                Console.WriteLine(node.Data);
+                node = node.Next; 
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+}
---
+++ LinkedListApp/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinkedListApp;
+
+
+
+/*Please not that generic linked List position starts at 0*/
+
+RunInsertExample();
+
+RunDeleteExample();
+
+Console.ReadLine();
+
+
+static void RunInsertExample()
+{
+    Console.WriteLine("***********INSERT EXAMPLE**********\r\n");
+    Console.WriteLine("---Initial List---");
+    var list = GetFruitsList();
+    list.PrintAll();
+
+    Console.WriteLine("---Inserting at specified position---");
+    list.InsertAt(2, "Blueberry");
+    list.InsertAt(4, "BlackBerry");
+        
+    Console.WriteLine("\r\n---Updated list after inserting new data at specified positions--");
+    list.PrintAll();
+
+}
+
+static void RunDeleteExample()
+{
+    Console.WriteLine("***********DELETE EXAMPLE**********\r\n");
+    Console.WriteLine("---Initial List---");
+    var list = GetFruitsList();
+    list.PrintAll();
+
+    Console.WriteLine("---Deleting at specified position---");
+    list.RemoveAt(2);
+    
+    Console.WriteLine("\r\n---Updated list after removing data at specified positions--");
+    list.PrintAll();
+
+}
+
+static GenericLinkedList<string> GetFruitsList()
+{
+    var list =  new GenericLinkedList<string>("Apple");
+    list.Insert("Orange");
+    list.Insert("Banana");
+    list.Insert("Grapes");
+    list.Insert("pineapple");
+
+    return list;
+}
+
+
